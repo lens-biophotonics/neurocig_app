3072ff5bae0c51bc478429d828ed752f4e34d592
Giacomo Mazzamuto <giacomo.mazzamuto@cnr.it>
2025-10-20 17:51:35 +0200

    Show frame, handle arrow keys

--- lib/app_web/live/video_live/form.ex
+++ lib/app_web/live/video_live/form.ex
@@ -8,18 +8,21 @@ defmodule AppWeb.VideoLive.Form do
   def render(assigns) do
     ~H"""
     <Layouts.app flash={@flash}>
-      <.header>
-        {@page_title}
-        <:subtitle>Use this form to manage video records in your database.</:subtitle>
-      </.header>
+      <div phx-window-keydown="key_event">
+        <.header>
+          {@video.name}
+        </.header>
 
-      <.form for={@form} id="video-form" phx-change="validate" phx-submit="save">
-        <.input field={@form[:name]} type="text" label="Name" />
-        <footer>
-          <.button phx-disable-with="Saving..." variant="primary">Save Video</.button>
-          <.button navigate={return_path(@return_to, @video)}>Cancel</.button>
-        </footer>
-      </.form>
+        <.form for={@form} id="video-form" phx-change="validate" phx-submit="save">
+          <.input field={@form[:name]} type="text" label="Name" /> Frame: {@frame}
+
+          <img src={@frame_path} />
+
+          <footer>
+            <.button navigate={return_path(@return_to, @video)}>Back</.button>
+          </footer>
+        </.form>
+      </div>
     </Layouts.app>
     """
   end
@@ -39,8 +42,8 @@ defmodule AppWeb.VideoLive.Form do
     video = Videos.get_video!(id)
 
     socket
-    |> assign(:page_title, "Edit Video")
     |> assign(:video, video)
+    |> assign_frame(1)
     |> assign(:form, to_form(Videos.change_video(video)))
   end
 
@@ -57,6 +60,21 @@ defmodule AppWeb.VideoLive.Form do
   def handle_event("validate", %{"video" => video_params}, socket) do
     changeset = Videos.change_video(socket.assigns.video, video_params)
     {:noreply, assign(socket, form: to_form(changeset, action: :validate))}
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event("key_event", %{"ctrlKey" => false, "key" => "ArrowRight"}, socket) do
+    {:noreply, inc_frame(socket)}
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event("key_event", %{"ctrlKey" => false, "key" => "ArrowLeft"}, socket) do
+    {:noreply, dec_frame(socket)}
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event("key_event", _params, socket) do
+    {:noreply, socket}
   end
 
   def handle_event("save", %{"video" => video_params}, socket) do
@@ -90,4 +108,34 @@ defmodule AppWeb.VideoLive.Form do
   end
 
   defp return_path("index", _video), do: ~p"/videos"
+
+  defp assign_frame(socket, frame) do
+    frame_string =
+      Integer.to_string(frame)
+      |> String.pad_leading(5, "0")
+
+    frame_path = "/videos/#{socket.assigns.video.name}_frames/#{frame_string}.jpg"
+
+    socket
+    |> assign(:frame, frame)
+    |> assign(:frame_path, frame_path)
+  end
+
+  defp inc_frame(socket) do
+    new_frame = socket.assigns.frame + 1
+
+    socket
+    |> assign_frame(new_frame)
+  end
+
+  defp dec_frame(socket) do
+    new_frame =
+      case socket.assigns.frame do
+        1 -> 1
+        _ -> socket.assigns.frame - 1
+      end
+
+    socket
+    |> assign_frame(new_frame)
+  end
 end
