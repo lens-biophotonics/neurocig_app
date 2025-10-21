bfbba06b9367a97850927fba900335b97f7ad1ed
Giacomo Mazzamuto <giacomo.mazzamuto@cnr.it>
2025-10-21 16:09:06 +0200

    disable longpoll

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,7 +27,6 @@ import topbar from "../vendor/topbar"
 
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 const liveSocket = new LiveSocket("/live", Socket, {
-    longPollFallbackMs: 2500,
     params: { _csrf_token: csrfToken },
     metadata: {
         keydown: (event, element) => {
