3918c686f3e23cf93cfdbb318ea23351a3019acf
Giacomo Mazzamuto <giacomo.mazzamuto@cnr.it>
2025-10-21 14:45:50 +0200

    remove old code

--- lib/app/annotations.ex
+++ lib/app/annotations.ex
@@ -6,109 +6,7 @@ defmodule App.Annotations do
   require Logger
 
   import Ecto.Query, warn: false
-  alias App.Repo
-
-  alias App.Videos.Video
   alias App.Annotations.Annotation
-
-  @doc """
-  Returns the list of annotations.
-
-  ## Examples
-
-      iex> list_annotations()
-      [%Annotation{}, ...]
-
-  """
-  def list_annotations do
-    Repo.all(Annotation)
-  end
-
-  @doc """
-  Gets a single annotation.
-
-  Raises `Ecto.NoResultsError` if the Annotation does not exist.
-
-  ## Examples
-
-      iex> get_annotation!(123)
-      %Annotation{}
-
-      iex> get_annotation!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_annotation!(id), do: Repo.get!(Annotation, id)
-
-  @doc """
-  Creates a annotation.
-
-  ## Examples
-
-      iex> create_annotation(%{field: value})
-      {:ok, %Annotation{}}
-
-      iex> create_annotation(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_annotation(attrs) do
-    %Annotation{}
-    |> Annotation.changeset(attrs)
-    |> Repo.insert()
-  end
-
-  @doc """
-  Updates a annotation.
-
-  ## Examples
-
-      iex> update_annotation(annotation, %{field: new_value})
-      {:ok, %Annotation{}}
-
-      iex> update_annotation(annotation, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_annotation(%Annotation{} = annotation, attrs) do
-    annotation
-    |> Annotation.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a annotation.
-
-  ## Examples
-
-      iex> delete_annotation(annotation)
-      {:ok, %Annotation{}}
-
-      iex> delete_annotation(annotation)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_annotation(%Annotation{} = annotation) do
-    Repo.delete(annotation)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking annotation changes.
-
-  ## Examples
-
-      iex> change_annotation(annotation)
-      %Ecto.Changeset{data: %Annotation{}}
-
-  """
-  def change_annotation(%Annotation{} = annotation, attrs \\ %{}) do
-    Annotation.changeset(annotation, attrs)
-  end
-
-  def get_annotations(%Video{} = video, frame) do
-    from(a in Annotation, where: a.video_id == ^video.id and a.frame == ^frame)
-    |> Repo.all()
-  end
 
   def load_annotations(video) do
     json =
@@ -133,7 +31,7 @@ defmodule App.Annotations do
     |> Map.new(fn mouse_id ->
       ann =
         %Annotation{
-          video_id: video.id,
+          video: video,
           frame: frame,
           mouse_id: String.to_integer(mouse_id),
           bb_x1: frame_data[mouse_id]["bbox"]["x1"],
--- lib/app/annotations/annotation.ex
+++ lib/app/annotations/annotation.ex
@@ -1,46 +1,19 @@
 defmodule App.Annotations.Annotation do
-  use Ecto.Schema
-  import Ecto.Changeset
+  alias App.Videos.Video
 
-  schema "annotations" do
-    belongs_to :video, App.Videos.Video
-    field :frame, :integer
-    field :mouse_id, :integer
-    field :bb_x1, :float
-    field :bb_y1, :float
-    field :bb_x2, :float
-    field :bb_y2, :float
-    field :nose_x, :float
-    field :nose_y, :float
-    field :earL_x, :float
-    field :earL_y, :float
-    field :earR_x, :float
-    field :earR_y, :float
-    field :tailB_x, :float
-    field :tailB_y, :float
-
-    timestamps(type: :utc_datetime)
-  end
-
-  @doc false
-  def changeset(annotation, attrs) do
-    annotation
-    |> cast(attrs, [
-      :video_id,
-      :frame,
-      :mouse_id,
-      :bb_x1,
-      :bb_y1,
-      :bb_x2,
-      :bb_y2,
-      :nose_x,
-      :nose_y,
-      :earL_x,
-      :earL_y,
-      :earR_x,
-      :earR_y,
-      :tailB_x,
-      :tailB_y
-    ])
-  end
+  defstruct video: %Video{},
+            frame: nil,
+            mouse_id: nil,
+            bb_x1: nil,
+            bb_y1: nil,
+            bb_x2: nil,
+            bb_y2: nil,
+            nose_x: nil,
+            nose_y: nil,
+            earL_x: nil,
+            earL_y: nil,
+            earR_x: nil,
+            earR_y: nil,
+            tailB_x: nil,
+            tailB_y: nil
 end
