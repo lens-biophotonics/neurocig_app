b1534ed23076b884c6c505cc1d066c0014939445
Giacomo Mazzamuto <giacomo.mazzamuto@cnr.it>
2025-11-18 16:19:06 +0100

    Fix home link

--- lib/app_web/components/layouts.ex
+++ lib/app_web/components/layouts.ex
@@ -37,7 +37,7 @@ defmodule AppWeb.Layouts do
     ~H"""
     <header class="navbar px-4 sm:px-6 lg:px-8">
       <div class="flex-1">
-        <a href="/" class="flex-1 flex w-fit items-center gap-2">
+        <a href="./" class="flex-1 flex w-fit items-center gap-2">
           <img src={~p"/images/logo.svg"} width="36" />
           <span class="text-sm font-semibold">Home</span>
         </a>
